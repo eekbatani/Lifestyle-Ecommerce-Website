ec69751abc087c3ae3826710e098404d42a56661
Ehsan Ekbatani <ehsanak@hotmail.com>
2019-11-15 23:29:04 -0500

    fixed alignment issue & removed unused copmonent

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './login/login.component';
 import { SuccessComponent } from './success/success.component';
 import { CartComponent } from './cart/cart.component';
-import { ListComponent } from './list/list.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { RouterModule } from '@angular/router';
 
@@ -21,7 +20,7 @@ import { RouterModule } from '@angular/router';
     LoginComponent,
     SuccessComponent,
     CartComponent,
-    ListComponent
+    
   ],
   imports: [
     BrowserModule,
